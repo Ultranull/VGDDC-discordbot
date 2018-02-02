d47c566de85278c9d95cf68c671677286f698916
jric <jrich006@odu.edu>
2018-02-01 22:01:57 -0500

    cleaned up and added sacrifice

--- bot.js
+++ bot.js
@@ -7,7 +7,6 @@ const sql = require('sqlite')
 sql.open('./score.sqlite')
 sql.open('./skills.sqlite')
 
-
 client.on('ready', () => {
     console.log('Logged in as ' + client.user.tag)
 })
@@ -21,60 +20,35 @@ client.on('message', message => {
     if (message.author.bot) return
     if (message.channel.type !== 'text') return
 
-    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
-        if (!row) {
-            sql.run(`INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)`, [message.author.id, 1, 0])
-        } else {
-            let curLevel = Math.floor(0.1 * Math.sqrt(row.points + 1))
-            if (curLevel > row.level) {
-                row.level = curLevel
-                sql.run(`UPDATE scores SET points = ${row.points + 1}, level = ${row.level} WHERE userId = ${message.author.id}`)
-                message.reply("You've leveled up to level " + curLevel + "! Ain't that dandy?")
-            }
-            sql.run(`UPDATE scores SET points = ${row.points + 1} WHERE userId = ${message.author.id}`)
-        }
-    }).catch(() => {
-        console.error
-        sql.run(`CREATE TABLE IF NOT EXISTS scores (userId TEXT, points INTEGER, level INTEGER)`).then(() => {
-            sql.run(`INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)`, [message.author.id, 1, 0])
-        })
-    })
+
     var banned = ['fuck', 'shit', 'cunt', 'dick']
-    var love = ['xoth', 'vhoorl', 'Trai Corte', 'octopus tentacle']
+    var replacements = ['xoth', 'vhoorl', 'Trai Corte', 'octopus tentacle']
 
     for (var i = 0; i < banned.length; i++) {
         if (msg.indexOf(banned[i]) >= 0) {
-            message.reply('hey thats a bad word! use ' + love[i] + ' instead!')
+            message.reply('hey thats a bad word! use ' + replacements[i] + ' instead!')
         }
     }
 
+
+
+    let sced = message.guild.roles.find('name', 'sacrificed');
+    let member = message.mentions.members.first();
     if (msg.substring(0, 1) === prefix) {
         switch (cmd) {
             case 'ping':
                 message.channel.send('pong!')
                 break
-            case 'level':
-                sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
-                    if (!row) return message.reply('Your current level is 0')
-                    message.reply('Your current level is ' + row.level)
-                })
-                break
-            case 'points':
-                sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
-                    if (!row) return message.reply('sadly you do not have any points yet!')
-                    message.reply('you currently have ' + row.points + ' points, good going!')
-                })
-                break
-            case 'reset':
-                sql.run(`DROP TABLE scores`)
-                console.log('dropping...')
-                message.reply('the deed is done.')
 
+            case 'sacrifice':
+                member.addRole(sced).catch(console.error);
+                message.channel.send(member.user.username + "'s soul has been consumed by the mighty lord!");
                 break
-            case 'test':
-                var sac = message.channel.members.get('username', args[0])
-                message.channel.send('hello')
-                break
+            case 'resurrect':
+                member.removeRole(sced).catch(console.error);
+                message.channel.send(member.user.username + "'s soul has been pulled from the underworld!");
+
+                break;
             case 'help':
                 message.channel.send('i cant help you ' + message.author.username)
                 break
@@ -82,11 +56,11 @@ client.on('message', message => {
                 message.channel.send(message.author.avatarURL)
                 break
             case 'banned':
-                var m = "";
-                m += ("these are the banned language: \n");
+                var m = ''
+                m += ('these are the banned language: \n')
                 for (var s = 0; s < banned.length; s++)
-                    m += (" -" + banned[s] + "\n");
-                message.channel.send(m);
+                    m += (' -' + banned[s] + '\n')
+                message.channel.send(m)
                 break
         }
     }
